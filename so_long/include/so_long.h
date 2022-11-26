5b76b9ffcb1ccd7ef54d7de27c5700791deb99df
Huitae Park <huipark@c10r8s9.42seoul.kr>
2022-11-26 16:59:37 +0900

    [so_long] the commit.

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,14 +6,25 @@
 /*   By: huipark <huipark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 17:09:47 by huipark           #+#    #+#             */
-/*   Updated: 2022/11/25 20:26:56 by huipark          ###   ########.fr       */
+/*   Updated: 2022/11/26 16:52:29 by huipark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "../mlx_mms/mlx.h"
+# define X_EVENT_KEY_PRESS			2
+# define X_EVENT_KEY_RELEASE		3
+
+# define KEY_ESC		53
+# define KEY_W			13
+# define KEY_A			0
+# define KEY_S			1
+# define KEY_D			2
+
+# include "../mlx_opengl/mlx.h"
+
+
 
 void	mlx_start(void *mlx, void *mlx_win);
 
